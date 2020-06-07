7b7b5b4e9ee76aca69ab317bae1846a3debcbdae
ArabGScom <66540674+ArabGScom@users.noreply.github.com>
2020-06-07 05:15:15 +0300

    Add files via upload

---
+++ AGS-AntiDDoS/fxmanifest.lua
@@ -0,0 +1,17 @@
+-- Manifest data
+fx_version 'bodacious'
+games {'gta5'}
+
+-- Resource stuff
+name 'AGS Anti DDoS'
+description 'Protect servers from denial of service attacks.'
+version 'v1.0'
+author 'AGS Team'
+url 'https://ArabGS.com'
+
+-- Do not change anything,
+-- If you change the information, the service will be stopped .
+-- See: https://antiddos.arabgs.com/Terms-of-Use-and-Privacy-Policy.html
+files {
+    'ags.cfg',
+}
